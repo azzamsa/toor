fdd0df1e5c15996e0e13d475a7590231ebdaa398
Kiryl Mialeshka <8974488+meskill@users.noreply.github.com>
2024-11-28 10:44:23 +0100

    fix build

--- src/project.rs
+++ src/project.rs
@@ -4,7 +4,7 @@ use crate::config::Config;
 
 /// A list of files considered to mark the root of a project.
 /// `https://github.com/bbatsov/projectile/blob/71f18add5e66201c3ea7c9650b848968db3aec42/projectile.el#L326`
-const PROJECT_ROOT_MARKS: [&str; 9] = [
+const PROJECT_ROOT_MARKS: &[&str] = &[
     ".project",    // Default project mark for Emacs
     ".projectile", // Default project mark for Projectile
     //
